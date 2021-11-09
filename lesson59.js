0bce0c81b7247671f6c85ee6350a69babcbcbdee
Dima <88742126+DimaWeb44@users.noreply.github.com>
2021-11-09 23:22:04 +0300

    Add files via upload

---
+++ lesson59.js
@@ -0,0 +1,17 @@
+
+var myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true
+  },
+  // Add record here
+  {
+    artist: "js",
+    title: "reakt",
+    release_year: 1997,
+    formats: ["CD", "tv", "tt"]
+  }
+];
